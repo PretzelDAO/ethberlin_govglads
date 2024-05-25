a7cb7fbd7a4233d6c556efee8c6cee4bc19d867d
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-05-25 12:30:52 +0200

    Fix header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,9 +4,11 @@ import "./header.scss";
 
 export default function Header() {
   return (
+    <div className="h-[calc(56px+10px+10px)]">
       <header>
-        <Image src={logo} alt="" height={55}/>
+        <Image src={logo} alt="" height={56} />
         <h1>Governance Gladiators</h1>
       </header>
+    </div>
   );
 }
