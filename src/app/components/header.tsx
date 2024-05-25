c9ad190fce14ecb08b1a63c6fb79ff9213f8d087
Christian Ziegler <c@ziegler.dev>
2024-05-26 00:16:31 +0200

    merge header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,12 +1,20 @@
-'use client';
+'use client'
 
 import Image from "next/image";
 import logo from "@/images/logo.png";
-import "./header.scss";
-import { useContext, useEffect, useState } from "react";
 import { NearContext } from "../contexts/WalletContext";
+import { useContext, useEffect, useState } from "react";
+import "./header.scss";
+import { Dao } from "@/domains/dao";
+import Toggle from "./toggle";
 
-export default function Header() {
+interface Props {
+  dao?: Dao;
+  setBerlin: (b: boolean) => void;
+  berlin: boolean;
+}
+
+export default function Header({ dao, setBerlin, berlin }: Props) {
   const { signedAccountId, wallet } = useContext(NearContext);
   const [action, setAction] = useState(() => { });
   const [label, setLabel] = useState('Loading...');
@@ -24,16 +32,28 @@ export default function Header() {
   }, [signedAccountId, wallet]);
 
   return (
-    <div className="h-[calc(56px+10px+10px)]">
+    <div className="h-[calc(56px+10px+10px)] bg-transparent">
       <header>
-        <Image src={logo} alt="" height={56} />
-        <h1>Governance Gladiators</h1>
-        { dao &&
+        <div className="flex flex-row justify-between bg-transparent">
+          <div>
+            <Image src={logo} alt="" height={56} />
+            <h1>Governance Gladiators</h1>
+            <button clasName="btn btn-secondary float-right pt-4" onClick={action as any}>{label}</button>
+          </div>
+          {dao && (
             <div className="welcome">
-                <img className="dao-logo" src={dao.logo} alt="DAO Logo" />
-                <h3>Welcome to {dao.name} Arena</h3>
+              <img className="dao-logo" src={dao.logo} alt="DAO Logo" />
+              <h3>Welcome to {dao.name} Arena</h3>
             </div>
-        }
+          )}
+
+          <Toggle
+            leftLabel="Berlin"
+            rightLabel="Arena"
+            onChange={(s) => setBerlin(s == "left")}
+            state={berlin ? "left" : "right"}
+          />
+        </div>
       </header>
     </div>
   );
