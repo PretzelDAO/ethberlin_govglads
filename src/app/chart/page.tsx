0431f515c5b970382faefbc7e44f0b5691d8d43a
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-05-25 18:32:31 +0200

    echart

---
+++ src/app/chart/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRef, useEffect } from "react";
+import * as echarts from "echarts";
+
+const Page = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const chart = echarts.init(ref.current);
+    chart.setOption({
+      title: {
+        text: "ECharts Getting Started Example",
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["shirt", "cardigan", "chiffon", "pants", "heels", "socks"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "sales",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
+  }, []);
+
+  return <div ref={ref} className="h-80"></div>;
+};
+
+export default Page;
