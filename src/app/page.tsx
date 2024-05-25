c8c282a5ddf3e72830b6de207e550be8623f1501
Bryan Fullam <bryanfullam@gmail.com>
2024-05-25 23:38:04 +0200

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import React, { useState } from 'react';
 
 export default function Home() {
   const [dao, setDao] = useState<Dao | undefined>(undefined);
-
   return (
     <main className="relative flex flex-col items-center justify-between p-24">
       {!dao && <Daos daoSelected={(d) => setDao(d)} />}
