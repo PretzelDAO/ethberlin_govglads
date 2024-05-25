0ff9563b2091b5a5a5185a3334857d67c98fde0a
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-05-25 19:02:56 +0200

    Clean up submit button

--- src/app/proposal/submit-button.tsx
+++ src/app/proposal/submit-button.tsx
@@ -1,3 +1,5 @@
+import composeClassName from "@/utils/compose-class-name";
+
 interface Props {
   disabled: boolean;
   onClick?: () => void;
@@ -9,12 +11,19 @@ const SubmitButton = ({ disabled, onClick }: Props) => {
       type="submit"
       disabled={disabled}
       onClick={onClick}
-      className={`w-full px-6 py-2 rounded-lg text-white font-bold focus:outline-none focus:ring-2 focus:ring-offset-2
-        ${
-          disabled
-            ? "bg-gray-400 cursor-not-allowed"
-            : "bg-blue-500 hover:bg-blue-600 focus:ring-blue-500"
-        }`}
+      className={composeClassName(
+        "w-full",
+        "px-6",
+        "py-2",
+        "rounded-lg",
+        "text-white",
+        "font-bold",
+        "focus:outline-none",
+        "focus:ring-2",
+        "focus:ring-offset-2",
+        disabled && "bg-gray-400 cursor-not-allowed",
+        !disabled && "bg-blue-500 hover:bg-blue-600 focus:ring-blue-500"
+      )}
     >
       Test proposal now
     </button>
