1c44567779f1910f9744d065d9e01f82e5d8aaaa
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-05-25 11:12:20 +0200

    Fix build

--- src/app/daos/page.tsx
+++ src/app/daos/page.tsx
@@ -5,111 +5,109 @@ import { Dao } from "@/domains/dao";
 import React, { useEffect, useState } from 'react';
 import "./daos.scss";
 
-const alldaos: [Dao] = [
+const alldaos: Dao[] = [
   {
-    "id": 1,
-    "name": "Uniswap",
-    "logo": "https://assets.coingecko.com/coins/images/12504/large/uniswap-uni.png"
+    id: "uniswap",
+    name: "Uniswap",
+    logo: "https://assets.coingecko.com/coins/images/12504/large/uniswap-uni.png",
   },
   {
-    "id": 2,
-    "name": "MakerDAO",
-    "logo": "https://assets.coingecko.com/coins/images/1364/large/Mark_Maker.png"
+    id: "maker-dao",
+    name: "MakerDAO",
+    logo: "https://assets.coingecko.com/coins/images/1364/large/Mark_Maker.png",
   },
   {
-    "id": 3,
-    "name": "Aave",
-    "logo": "https://assets.coingecko.com/coins/images/12645/large/AAVE.png"
+    id: "aave",
+    name: "Aave",
+    logo: "https://assets.coingecko.com/coins/images/12645/large/AAVE.png",
   },
   {
-    "id": 4,
-    "name": "Compound",
-    "logo": "https://assets.coingecko.com/coins/images/10775/large/COMP.png"
+    id: "compound",
+    name: "Compound",
+    logo: "https://assets.coingecko.com/coins/images/10775/large/COMP.png",
   },
   {
-    "id": 5,
-    "name": "SushiSwap",
-    "logo": "https://assets.coingecko.com/coins/images/12271/large/sushiswap-512x512.png"
+    id: "sushi-swap",
+    name: "SushiSwap",
+    logo: "https://assets.coingecko.com/coins/images/12271/large/sushiswap-512x512.png",
   },
   {
-    "id": 6,
-    "name": "Curve DAO",
-    "logo": "https://assets.coingecko.com/coins/images/12124/large/Curve.png"
+    id: "curve-dao",
+    name: "Curve DAO",
+    logo: "https://assets.coingecko.com/coins/images/12124/large/Curve.png",
   },
   {
-    "id": 7,
-    "name": "Yearn Finance",
-    "logo": "https://assets.coingecko.com/coins/images/11849/large/yearn-finance-yfi.png"
+    id: "yearn-finance",
+    name: "Yearn Finance",
+    logo: "https://assets.coingecko.com/coins/images/11849/large/yearn-finance-yfi.png",
   },
   {
-    "id": 8,
-    "name": "Balancer",
-    "logo": "https://assets.coingecko.com/coins/images/11683/large/Balancer.png"
+    id: "balancer",
+    name: "Balancer",
+    logo: "https://assets.coingecko.com/coins/images/11683/large/Balancer.png",
   },
   {
-    "id": 9,
-    "name": "PancakeSwap",
-    "logo": "https://assets.coingecko.com/coins/images/12632/large/pancakeswap-cake-logo.png"
-  }
+    id: "pancake-swap",
+    name: "PancakeSwap",
+    logo: "https://assets.coingecko.com/coins/images/12632/large/pancakeswap-cake-logo.png",
+  },
 ];
 
 interface DaoComponentProps {
-    dao: Dao;
+  dao: Dao;
 }
 
 function DaoComponent(props: DaoComponentProps) {
   const dao = props.dao;
   return (
     <div className="dao">
-        <img src={dao.logo} alt="Dao logo"/>
-        <h4>{dao.name}</h4>
+      <img src={dao.logo} alt="Dao logo" />
+      <h4>{dao.name}</h4>
     </div>
   );
 }
 
-interface DaosProps {
-    daoSelected: (dao: Dao) => void;
-}
-
-export default function Daos({ daoSelected }: DaosProps) {
+function Daos() {
   const [daos, setDaos] = useState<Dao[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
+  useEffect(() => {
     setDaos(alldaos);
     setLoading(false);
-//       fetch.get('/daos')
-//         .then(response => {
-//           setDaos(response.data);
-//           setLoading(false);
-//         })
-//         .catch(error => {
-//           setError('Failed to fetch DAOs');
-//           setLoading(false);
-//         });
-    }, []);
+    //       fetch.get('/daos')
+    //         .then(response => {
+    //           setDaos(response.data);
+    //           setLoading(false);
+    //         })
+    //         .catch(error => {
+    //           setError('Failed to fetch DAOs');
+    //           setLoading(false);
+    //         });
+  }, []);
 
-      if (loading) {
-        return <div>Loading...</div>;
-      }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-      if (error) {
-        return <div>{error}</div>;
-      }
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <div className="daos">
-    <h2>Select your DAO:</h2>
-    <div className="grid grid-cols-4 gap-4">
-      {
-      daos.map((d) => <DaoComponent
-        dao={d}
-        key={d.id}
-        onClick={() => daoSelected(d)}
-      />)
-      }
-    </div>
+      <h2>Select your DAO:</h2>
+      <div className="grid grid-cols-4 gap-4">
+        {daos.map((d) => (
+          <DaoComponent
+            dao={d}
+            key={d.id}
+            // onClick={() => daoSelected(d)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
+export default Daos;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/app/components/header.tsx";
+import Header from "@/app/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
