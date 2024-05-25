5843f080b986ef164efbf264226257404c4e2680
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-05-25 22:02:14 +0200

    Add toggle button

--- src/app/proposal/proposal.tsx
+++ src/app/proposal/proposal.tsx
@@ -10,6 +10,7 @@ import type { Dao } from "@/domains/dao";
 import SubmitButton from "./submit-button";
 import { DelegateContext } from "@/providers/stateProvider";
 import Results from "../results/results";
+import Toggle from "../components/toggle";
 
 interface ProposalProps {
   dao: Dao;
@@ -32,8 +33,15 @@ const Proposal = ({ dao, onSubmit }: ProposalProps) => {
 
   const disabled = !proposal;
 
-  if(showResults){
-    return <Results dao={dao} delegateProbabilities={selectedDelegateProbabilities} showScores={true} onChange={setSelectedDelegateProbabilities} />
+  if (showResults) {
+    return (
+      <Results
+        dao={dao}
+        delegateProbabilities={selectedDelegateProbabilities}
+        showScores={true}
+        onChange={setSelectedDelegateProbabilities}
+      />
+    );
   }
 
   return (
@@ -41,18 +49,26 @@ const Proposal = ({ dao, onSubmit }: ProposalProps) => {
       <img className="dao-logo" src={dao.logo} alt="DAO Logo" />
       <h3>1. Test a new proposal on &quot;{dao.name}&quot; DAO:</h3>
       <div className="row row-auto">
-      <textarea
-        value={proposal}
-        onChange={(e) => setProposal(e.target.value)}
-        className="w-full px-3 py-3 bg-slate-100 rounded-md"
-        placeholder="Type your new proposal..."
-      />
-      <button onClick={()=>{
-        dCon.setDelegates(dCon.delegates.map((d)=>{
-          return {...d, score: 0}
-          }
-        ));
-      }}>Reset</button>
+        <textarea
+          value={proposal}
+          onChange={(e) => setProposal(e.target.value)}
+          className="w-full px-3 py-3 bg-slate-100 rounded-md"
+          placeholder="Type your new proposal..."
+        />
+        <button
+          onClick={() => {
+            dCon.setDelegates(
+              dCon.delegates.map((d) => {
+                return { ...d, score: 0 };
+              })
+            );
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div className="flex justify-center">
+        <Toggle leftLabel="aaa" rightLabel="bbb" />
       </div>
       <h3>2. Select your delegates:</h3>
       <Delegates
@@ -64,29 +80,36 @@ const Proposal = ({ dao, onSubmit }: ProposalProps) => {
       {score !== -1 && (
         <p>The probability of your proposal passing is: {score}</p>
       )}
-      <SubmitButton disabled={disabled} onClick={()=>{
-        submitProposal(dao.id, {
-          proposal, delegates: dCon.selectedDelegates}).then((response: ProposalResponse) => {
-          // setScore(response.score);
-          const delProbs:any={}
-          response.probabilities.forEach((d)=>{
-            delProbs[d.voter]=d.similarity_ratio;
-          });
-          // dCon.setFinalResults(response);
-          dCon.setDelegates(dCon.delegates.map((d)=>{
-            return {...d, score: delProbs[d.wallet]}
-            }));
-          console.log("SET RES",response);
+      <SubmitButton
+        disabled={disabled}
+        onClick={() => {
+          submitProposal(dao.id, {
+            proposal,
+            delegates: dCon.selectedDelegates,
+          })
+            .then((response: ProposalResponse) => {
+              // setScore(response.score);
+              const delProbs: any = {};
+              response.probabilities.forEach((d) => {
+                delProbs[d.voter] = d.similarity_ratio;
+              });
+              // dCon.setFinalResults(response);
+              dCon.setDelegates(
+                dCon.delegates.map((d) => {
+                  return { ...d, score: delProbs[d.wallet] };
+                })
+              );
+              console.log("SET RES", response);
 
-          setLoading(false);
-          setShowResults(true);
-          
-          }).catch((error) => {
-            console.error(error);
-            setLoading(false);
-          }
-        );
-      }} />
+              setLoading(false);
+              setShowResults(true);
+            })
+            .catch((error) => {
+              console.error(error);
+              setLoading(false);
+            });
+        }}
+      />
     </div>
   );
 };
