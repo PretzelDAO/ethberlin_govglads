883f35ecdee1a2edff4abe555678d4a04ec581f9
TachysHeurisko <crypto@j-loewe.de>
2024-05-25 21:54:26 +0200

    fix results page further

--- src/app/results/results.tsx
+++ src/app/results/results.tsx
@@ -9,6 +9,7 @@ import Loading from "@/app/components/loading";
 import type { DelegateProbability } from "@/domains/proposal";
 import { DelegateContext } from "@/providers/stateProvider";
 import DelegateCluster from "../delegates/delegate_cluster";
+import { makeNiceAddress } from "@/utils/stringutils";
 
 interface DelegatesProps {
   dao: Dao;
@@ -17,16 +18,20 @@ interface DelegatesProps {
   onChange: (DelegateProbability: DelegateProbability[]) => void;
 }
 
-const Results = ({ dao, delegateProbabilities, showScores, onChange }: DelegatesProps) => {
+const Results = ({
+  dao,
+  delegateProbabilities,
+  showScores,
+  onChange,
+}: DelegatesProps) => {
   const [search, setSearch] = useState<string>("");
   // const [delegates, setDelegates] = useState<Delegate[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const dCon = useContext(DelegateContext);
-  
 
   if (loading) {
-    return <Loading msg={`Loading delegates for DAO ${dao.name}`}/>;
+    return <Loading msg={`Loading delegates for DAO ${dao.name}`} />;
   }
 
   if (error) {
@@ -34,21 +39,52 @@ const Results = ({ dao, delegateProbabilities, showScores, onChange }: Delegates
   }
 
   const toggled = (delegate: Delegate, newState: number) => {
-    let newDps = delegateProbabilities.filter(d => d.wallet !== delegate.wallet);
+    let newDps = delegateProbabilities.filter(
+      (d) => d.wallet !== delegate.wallet
+    );
     if (newState !== 0) {
-        newDps.push({
-            wallet: delegate.wallet,
-            probability: newState
-        });
+      newDps.push({
+        wallet: delegate.wallet,
+        probability: newState,
+      });
     }
     onChange(newDps);
   };
   console.log(dCon.delegates);
 
   return (
-        <div>
-            <DelegateCluster />
+    <div className="px-2">
+      <div className="h-1/2 overflow-hidden">
+        <DelegateCluster />
+      </div>
+      <div className="flex flex-row">
+        <div className="flex flex-col w-1/2">
+          <h3>Delegates voting for</h3>
+          <div className="flex flex-col px-2">
+            {dCon.delegates
+              .filter((d) => d.votingpower / d.maxvotingpower > 0.05)
+              .filter((d) => (d.score ?? 0) > 0)
+              .map((d) => {
+                return (
+                  <div
+                    key={d.wallet}
+                    className="bg-green-400 my-0.5 rounded-md"
+                  >
+                    <p>
+                      {d.name ?? makeNiceAddress(d.wallet)} (
+                      {Math.round(d.votingpower / 1000)}
+                      k)
+                    </p>
+                    <p>
+                      Probability: {Math.ceil(Math.abs(d.score ?? 0) * 100)}%
+                    </p>
+                  </div>
+                );
+              })}
+          </div>
         </div>
+      </div>
+    </div>
   );
 };
 
