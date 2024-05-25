811b29d120cfb2ac6bc4558d1171f7b866ee88e4
kosmotrade <dev@thechronotrade.com>
2024-05-25 09:12:35 +0200

    Add header and logo

---
+++ src/app/components/header.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import logo from "@/images/logo.png";
+import "./header.scss";
+
+export default function Header() {
+  return (
+      <header>
+        <Image src={logo} alt="" height={55}/>
+        <h1>Governance Gladiators</h1>
+      </header>
+  );
+}
